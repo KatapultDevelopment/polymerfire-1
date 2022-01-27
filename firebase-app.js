401b38415cb7ebbd626584494f495ffbdf9730ad
Josh Coldsmith <65235172+jdcoldsmith@users.noreply.github.com>
2022-01-27 15:21:31 +0000

    Updated firebase to version 9

--- firebase-app.js
+++ firebase-app.js
@@ -15,14 +15,13 @@ https://github.com/firebase/polymerfire/blob/master/LICENSE
  // Polymer imports
 import '@polymer/polymer/polymer-legacy.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-// import firebase from '@firebase/app';
-import '@firebase/app';
-import '@firebase/database';
-import '@firebase/auth';
-import '@firebase/storage';
-import '@firebase/messaging';
-import '@firebase/functions';
-import '@firebase/analytics';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+import 'firebase/compat/messaging';
+import 'firebase/compat/functions';
+import 'firebase/compat/analytics';
 import '../../source/_resources/js/firebaseWorker/firebaseWorker.js';
 
 /**
--- firebase-auth.js
+++ firebase-auth.js
@@ -59,9 +59,8 @@ be a problem.
 // Polymer imports
 import '@polymer/polymer/polymer-legacy.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-// import firebase from '@firebase/app';
 import { FirebaseCommonBehavior } from './firebase-common-behavior.js';
-import '@firebase/auth';
+import 'firebase/compat/auth';
 
 Polymer({
 
